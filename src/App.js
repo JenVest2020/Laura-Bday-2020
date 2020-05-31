ee0123feb257c1d8b0c025429a2c52468e690e67
JenVest2020 <jenvest2019@gmail.com>
2020-05-31 01:18:48 -0500

    working on Navigation component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Navigation from './components/Navigation';
 
 const AppDiv = styled.div`
 text-align: center;
-background-image:linear-gradient(red, orange,  yellow, green, blue, purple);
+background-image:linear-gradient(#990000, orange,  #cccc00, green, blue, purple);
 width: 1000px;
 height: 1000px;
+margin: 0 auto;
 `;
 
 const AppHeader = styled.h1`
-background-color: rgba(255, 0, 255, .3);
+background-color: rgba(255, 255, 255, .3);
  margin: 0;
   display: flex;
   flex-direction: column;
@@ -32,7 +33,7 @@ function App() {
     return (
         <AppDiv>
             <Navigation />
-            <AppHeader>Laura Stecher's Art Works</AppHeader>
+            <AppHeader>Mom's Favorites of Laura Stecher's Art Works</AppHeader>
             <AppSubheader>Click Any Thumbnail to View Full-Size</AppSubheader>
         </AppDiv>
     );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import styled from "styled-components";
+
+// const menuheader = styled.h2`
+//     text-align:right;
+//     font-size: 3rem;
+//     font-weight:bolder;
+// `;
 
 
 
@@ -10,8 +17,9 @@ function Navigation() {
 
     return (
         <div>
-            <Navbar color="muted" dark>
-                <NavbarBrand href="/" className="mr-auto">ART MENU</NavbarBrand>
+            <h6 className="text-white float-right">MENU</h6>
+            <Navbar color="muted" dark >
+                <NavbarBrand href="/" className="mr-auto float-right"></NavbarBrand>
                 <NavbarToggler onClick={toggleNavbar} className="mr-2" />
                 <Collapse isOpen={!collapsed} navbar>
                     <Nav navbar>
