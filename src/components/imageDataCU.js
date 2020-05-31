02d0523993a9912359ba60b545126d21c53f027a
JenVest2020 <jenvest2019@gmail.com>
2020-05-31 16:51:40 -0500

    working on imageDataCU file

---
+++ src/components/imageDataCU.js
@@ -0,0 +1,46 @@
+const imageDataCU = [
+    {
+        id: '1',
+        name: 'babyPinecones',
+        imageSrc: './src/components/close-ups/baby-pinecones-042119.jpg',
+        alt: 'close up of open hand holding a tiny branch of pine cone buds',
+    },
+    {
+        id: '2',
+        name: 'beaverpondTrail3',
+        imageSrc: './src/components/close-ups/beaverpond-trail3.jpg',
+        alt: 'close up of open bright yellow flower and stamen',
+    },
+    {
+        id: '3',
+        name: 'beaverpond trail5',
+        imageSrc: './src/components/close-ups/beaverpond-trail5.jpg',
+        alt: 'clos up of a brown snail in his shell on a curvy log',
+    },
+    {
+        id: '4',
+        name: 'beaverpond trail6',
+        imageSrc: './src/components/close-ups/beaverpond-trail6.jpg',
+        alt: 'close up of 3 wild blue long stemmed flowers',
+    },
+    {
+        id: '5',
+        name: 'beverpond trail7',
+        imageSrc: './src/components/close-ups/beaverpond-trail7.jpg',
+        alt: 'close up of a green plant with ten long cupped leaves in a star shape holding morning dew drops',
+    },
+    {
+        id: '6',
+        name: 'beaverpond trail9',
+        imageSrc: './src/components/close-ups/beaverpond-trail9.jpg',
+        alt: 'close up of a tiny ladybug on a long spikey leafed plant',
+    },
+    {
+        id: '7',
+        name: 'beaverpond trail10',
+        imageSrc: './src/components/close-ups/beaverpond-trail10.jpg',
+        alt: 'close up of some flower-like green plants on the winter thawed ground',
+    },
+];
+
+export default imageDataCU;
